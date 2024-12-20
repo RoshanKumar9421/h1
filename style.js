9c34eeef5c72a4f650abdfb0b9cd4b936cfa8ee6
Roshan kumar <yadavroshankumar8271@gmail.com>
2024-12-20 10:35:38 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,20 @@
+// Show the login options modal
+function showLoginOptions() {
+    document.getElementById("login-options").style.display = "block";
+}
+
+// Close the login options modal
+function closeLoginOptions() {
+    document.getElementById("login-options").style.display = "none";
+}
+
+// Redirect to Farmer Login
+function redirectToFarmerLogin() {
+    window.location.href = "login web page/farmer.html";
+}
+
+// Redirect to Consumer Login
+function redirectToConsumerLogin() {
+    window.location.href = "login web page/consumer.html";
+}
+
